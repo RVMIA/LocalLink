5fc91c2ac80bd615e47eca3ac1a9a73c96e4f4fc
Elliot Duncan <elliotdtx@gmail.com>
2025-04-14 22:49:58 -0500

    cleaned and polished Main.java

--- Main.java
+++ Main.java
@@ -4,20 +4,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("WELCOME TO LOCALLINK");
-        Scanner sc = new Scanner(System.in); // gets input
-        setLocation(sc); // sets home location on startup
+        Scanner scanner = new Scanner(System.in); // gets input
+        setLocation(scanner); // sets home location on startup
 
         Directory.readList(); // read list from database file
-        while (!menu(sc)); // until quit, keep prompting from menu
+        while (!processUserCommand(scanner)) ; // until quit, keep prompting from menu
 
-        sc.close();
+        scanner.close();
         System.out.println("\033[H\033[2J THANK YOU FOR USING LOCALLINK");
     }
 
-    public static boolean menu(Scanner sc) {
+    public static boolean processUserCommand(Scanner scanner) {
         Directory.saveList(); // saves the current list to the database file
         System.out.println("Type a command; type \"help\" for more info; type \"quit\" to quit");
-        String input = sc.nextLine();
+        String input = scanner.nextLine();
         switch (input) {
             case "quit":
                 Directory.saveList();
@@ -26,25 +26,25 @@ public class Main {
                 printHelp();
                 break;
             case "list":
-                list(sc);
+                list(scanner);
                 break;
             case "add":
-                addBusiness(sc);
+                addBusiness(scanner);
                 break;
             case "remove":
-                removeBusiness(sc);
+                removeBusiness(scanner);
                 break;
             case "review":
-                leaveReview(sc);
+                leaveReview(scanner);
                 break;
             case "maps":
-                maps(sc);
+                maps(scanner);
                 break;
             case "view":
-                view(sc);
+                view(scanner);
                 break;
             case "relocate":
-                setLocation(sc);
+                setLocation(scanner);
                 break;
             default:
                 System.out.println("not a valid command");
@@ -67,78 +67,62 @@ public class Main {
         System.out.println("\t\"maps\": opens the business in google maps in your browser");
     }
 
-    public static void maps(Scanner sc) {
-        System.out.println("name of business");
-        String n = sc.nextLine();
-        Business business = search(n);
-        if (business == null) {
-            System.out.println("unable to find business with that name");
-            return;
-        }
-        System.out.println("Opening maps");
-        business.openMaps();
-
+    public static Business search(String name) {
+        return Directory.getBusinesses()
+                .stream()
+                .filter(b -> b.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
     }
 
-    public static void list(Scanner sc) {
-        Comparator<Business> comp = null;
-        while (comp == null) {
-            System.out.println("sort by (name, distance, rating)");
-            String mode = sc.nextLine();
-            switch (mode) { // picks which way to sort the list of businesses
-                case "name":
-                    comp=Directory.NAME_COMPARATOR;
-                    break;
-                case "distance":
-                    comp=Directory.DIST_COMPARATOR;
-                    break;
-                case "rating":
-                    comp=Directory.RATING_COMPARATOR;
-                    break;
-                default:
-                    System.out.println("try again");
+    public static Business getBusinessFromInput(Scanner scanner){
+        System.out.println("name of business");
+        String name = scanner.nextLine();
+        return search(name);
+    }
 
+    public static Business getCoords(Scanner scanner) {
+        while (true) {
+            try {
+                System.out.println("coordinates as signed doubles, space separated");
+                System.out.println("(North and East are positive, South and West are negative)");
+                double latitude = scanner.nextDouble();
+                double longitude = scanner.nextDouble();
+                scanner.nextLine();
+                return new Business(latitude, longitude);
+
+            } catch (Exception e) {
+                System.err.println("invalid input");
+                scanner.nextLine();
             }
         }
-        for (Business b : Directory.getBusinesses(comp)) {
-            System.out.println(b);
-        }
-    }
-
-    public static void view(Scanner sc) {
-        System.out.println("name of business");
-        String n = sc.nextLine();
-        Business business = search(n);
-        if (business == null) {
-            System.out.println("unable to find business with that name");
-            return;
-        }
-        ArrayList<String> review = business.getReviews();
-        String a = (!review.isEmpty()) ? "|\t" + review.get(0) + "\n" : "";
-        String b = (review.size() > 1) ? "|\t" + review.get(1) + "\n" : "";
-        String c = (review.size() > 2) ? "|\t" + review.get(2) + "\n" : "";
-        System.out.printf("%s\n%.1f stars - %d reviews\n%s%s%s", business.getName(), business.getStarRating(),
-                business.getReviewCount(), a, b, c);
 
     }
 
-    public static void removeBusiness(Scanner sc) {
-        System.out.println("name of business");
-        String n = sc.nextLine();
-        Business b = search(n);
-        if (b == null) {
+    public static void addBusiness(Scanner scanner) {
+        System.out.println("name: ");
+        String name = scanner.nextLine();
+        Business newBusiness = getCoords(scanner);
+        newBusiness.setName(name);
+
+        Directory.addBusiness(newBusiness);
+        System.out.println("adding business to list: " + newBusiness);
+
+    }
+
+    public static void removeBusiness(Scanner scanner) {
+        Business businessToRemove = getBusinessFromInput(scanner);
+        if (businessToRemove == null) {
             System.out.println("unable to find business with that name");
             return;
         }
-        System.out.printf("removing business \"%s\"\n", n);
-        Directory.removeBusiness(b);
+        System.out.printf("removing business \"%s\"\n", businessToRemove.getName());
+        Directory.removeBusiness(businessToRemove);
     }
 
     public static void leaveReview(Scanner sc) {
-        System.out.println("name of business"); // searches for business
-        String n = sc.nextLine();
-        Business b = search(n);
-        if (b == null) {
+        Business businessToReview = getBusinessFromInput(sc);
+        if (businessToReview == null) {
             System.out.println("unable to find business with that name");
             return;
         }
@@ -159,55 +143,71 @@ public class Main {
 
         System.out.println("leave note here:"); // leave a comment
         String review = sc.nextLine();
-        b.addReview(stars, stars + " stars - " + review);
+        businessToReview.addReview(stars, stars + " stars - " + review);
 
     }
 
-    // returns the business where the names match
-    public static Business search(String n) {
-        for (Business b : Directory.getBusinesses()) {
-            if (b.getName().equals(n)) {
-                return b;
-            }
-        }
-        return null;
-    }
-    // asks for coordinates dealing with error handling, returning a temporary business that only stores coordinates.
-    public static Business getCoords(Scanner sc){
-        while (true) {
-            try {
-                System.out.println("coordinates as signed doubles, space separated");
-                System.out.println("(North and East are positive, South and West are negative)");
-                double lat = sc.nextDouble();
-                double lon = sc.nextDouble();
-                sc.nextLine();
-                return new Business(lat, lon);
-
-            } catch (Exception e) {
-                System.err.println("invalid input");
-                sc.nextLine();
-            }
+    public static void setLocation(Scanner sc) {
+        try {
+            Business temp = getCoords(sc); // get coords
+            Directory.setUserLocation(temp.getCoordsX(), temp.getCoordsY());
+            System.out.printf("Setting coordinates to (%.4f, %.4f)\n", temp.getCoordsX(), temp.getCoordsY());
+        } catch (Exception e) {
+            System.err.println("invalid input");
+            sc.nextLine();
         }
 
     }
-    // adds a new business to the list, asking for a name and coords
-    public static void addBusiness(Scanner sc) {
-        System.out.println("name: ");
-        String name = sc.nextLine();
-        Business b = getCoords(sc);
-        b.setName(name);
 
-        Directory.addBusiness(b);
-        System.out.println("adding business to list: " + b);
-
-    }
-
-    // sets the "home" location in the program
-    public static void setLocation(Scanner sc) throws InputMismatchException {
-        Business temp = getCoords(sc); // get coords
-        Directory.setUserLocation(temp.getCoordsX(), temp.getCoordsY());
-        System.out.printf("Setting coordinates to (%.4f, %.4f)\n", temp.getCoordsX(), temp.getCoordsY());
+    public static void maps(Scanner sc) {
+        Business businessToView = getBusinessFromInput(sc);
+        if (businessToView == null) {
+            System.out.println("unable to find business with that name");
+            return;
+        }
+        System.out.println("Opening maps");
+        businessToView.openMaps();
 
     }
 
+    public static void list(Scanner sc) {
+        Comparator<Business> businessComparator = null;
+        while (businessComparator == null) {
+            System.out.println("sort by (name, distance, rating)");
+            String mode = sc.nextLine();
+            switch (mode) { // picks which way to sort the list of businesses
+                case "name":
+                    businessComparator = Directory.NAME_COMPARATOR;
+                    break;
+                case "distance":
+                    businessComparator = Directory.DIST_COMPARATOR;
+                    break;
+                case "rating":
+                    businessComparator = Directory.RATING_COMPARATOR;
+                    break;
+                default:
+                    System.out.println("try again");
+
+            }
+        }
+        for (Business b : Directory.getBusinesses(businessComparator)) {
+            System.out.println(b);
+        }
+    }
+
+    public static void view(Scanner sc) {
+        Business business = getBusinessFromInput(sc);
+        if (business == null) {
+            System.out.println("unable to find business with that name");
+            return;
+        }
+
+        ArrayList<String> review = business.getReviews();
+        String a = (!review.isEmpty()) ? "|\t" + review.get(0) + "\n" : "";
+        String b = (review.size() > 1) ? "|\t" + review.get(1) + "\n" : "";
+        String c = (review.size() > 2) ? "|\t" + review.get(2) + "\n" : "";
+        System.out.printf("%s\n%.1f stars - %d reviews\n%s%s%s", business.getName(), business.getStarRating(),
+                business.getReviewCount(), a, b, c);
+
+    }
 }
